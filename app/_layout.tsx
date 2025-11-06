464807786bfbcbf5e990121736b0d59dd10bb60b
eriker75 <eribertmarquez75@gmail.com>
2025-11-06 16:10:55 -0400

    useFonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,48 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from "react-native-safe-area-context";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Black": require("../assets/fonts/roboto/Roboto-Black.ttf"),
+    "Roboto-BlackItalic": require("../assets/fonts/roboto/Roboto-BlackItalic.ttf"),
+    "Roboto-Bold": require("../assets/fonts/roboto/Roboto-Bold.ttf"),
+    "Roboto-BoldItalic": require("../assets/fonts/roboto/Roboto-BoldItalic.ttf"),
+    "Roboto-ExtraBold": require("../assets/fonts/roboto/Roboto-ExtraBold.ttf"),
+    "Roboto-ExtraBoldItalic": require("../assets/fonts/roboto/Roboto-ExtraBoldItalic.ttf"),
+    "Roboto-ExtraLight": require("../assets/fonts/roboto/Roboto-ExtraLight.ttf"),
+    "Roboto-ExtraLightItalic": require("../assets/fonts/roboto/Roboto-ExtraLightItalic.ttf"),
+    "Roboto-Italic": require("../assets/fonts/roboto/Roboto-Italic.ttf"),
+    "Roboto-Light": require("../assets/fonts/roboto/Roboto-Light.ttf"),
+    "Roboto-LightItalic": require("../assets/fonts/roboto/Roboto-LightItalic.ttf"),
+    "Roboto-Medium": require("../assets/fonts/roboto/Roboto-Medium.ttf"),
+    "Roboto-MediumItalic": require("../assets/fonts/roboto/Roboto-MediumItalic.ttf"),
+    "Roboto-Regular": require("../assets/fonts/roboto/Roboto-Regular.ttf"),
+    "Roboto-SemiBold": require("../assets/fonts/roboto/Roboto-SemiBold.ttf"),
+    "Roboto-SemiBoldItalic": require("../assets/fonts/roboto/Roboto-SemiBoldItalic.ttf"),
+    "Roboto-Thin": require("../assets/fonts/roboto/Roboto-Thin.ttf"),
+    "Roboto-ThinItalic": require("../assets/fonts/roboto/Roboto-ThinItalic.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <GluestackUIProvider mode="dark">
       <SafeAreaProvider initialMetrics={initialWindowMetrics}>
