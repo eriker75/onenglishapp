f8b1de65b97525b57ff1f5605cf18873c85a6313
eriker75 <eribertmarquez75@gmail.com>
2025-11-07 15:36:48 -0400

    dashboard

--- app/dashboard/home.tsx
+++ app/dashboard/home.tsx
@@ -1,14 +1,24 @@
-import { Text } from "@/components/ui";
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Box } from "@/components/ui";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreenView = () => {
   return (
-    <SafeAreaView className="bg-[#1D286E]">
-      {/* Your home screen content goes here */}
-      <Text className="text-white">Welcome to the Home Screen!</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <LinearGradient
+        colors={["#2C93FE", "#FFFFFF"]}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.5 }}
+        style={{ flex: 1 }}
+      >
+        <View className="flex-1">
+          <Box className="bg-white p-4 h-16 rounded-br-[40px] rounded-bl-[40px]"></Box>
+        </View>
+      </LinearGradient>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreenView
+export default HomeScreenView;
