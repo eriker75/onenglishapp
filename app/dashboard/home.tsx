03c68ef16730d1ca14bff299440331be57b5223c
eriker75 <eribertmarquez75@gmail.com>
2025-11-07 17:00:54 -0400

    safe area recorted

--- app/dashboard/home.tsx
+++ app/dashboard/home.tsx
@@ -1,22 +1,19 @@
 import HeaderMenu from "@/components/blocs/HeaderMenu";
 import { LinearGradient } from "expo-linear-gradient";
 import { View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreenView = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <LinearGradient
-        colors={["#2C93FE", "#FFFFFF"]}
-        start={{ x: 0.5, y: 0 }}
-        end={{ x: 0.5, y: 0.5 }}
-        style={{ flex: 1 }}
-      >
-        <View className="flex-1">
-          <HeaderMenu />
-        </View>
-      </LinearGradient>
-    </SafeAreaView>
+    <LinearGradient
+      colors={["#2C93FE", "#FFFFFF"]}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 0.5 }}
+      style={{ flex: 1 }}
+    >
+      <View className="flex-1">
+        <HeaderMenu />
+      </View>
+    </LinearGradient>
   );
 };
 
